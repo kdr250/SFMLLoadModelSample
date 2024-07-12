18d017487633a04675f230f791c2760c19b5ae1b
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-07-12 13:25:05 +0900

    VertexArrayを使うよう修正

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+constexpr int WINDOW_WIDTH  = 1280;
+constexpr int WINDOW_HEIGHT = 768;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1280, 768), "Shader Smaple", sf::Style::Titlebar | sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+                            "Shader Smaple",
+                            sf::Style::Titlebar | sf::Style::Close);
 
     sf::Shader shader;
-    if (!shader.loadFromFile("resources/shader/shader.vert", "resources/shader/shader.frag"))
+    if (!shader.loadFromFile("resources/shader/shader.frag", sf::Shader::Fragment))
     {
         std::cerr << "failed to load shaders" << std::endl;
         return -1;
     }
 
-    sf::RectangleShape rect {sf::Vector2f(window.getSize())};
+    sf::VertexArray vertices(sf::TriangleStrip, 3);
+    vertices[0].position = sf::Vector2f(WINDOW_WIDTH * 0.5f, WINDOW_HEIGHT * 0.2f);
+    vertices[0].color    = sf::Color::Red;
+    vertices[1].position = sf::Vector2f(WINDOW_WIDTH * 0.8f, WINDOW_HEIGHT * 0.8f);
+    vertices[1].color    = sf::Color::Green;
+    vertices[2].position = sf::Vector2f(WINDOW_WIDTH * 0.2f, WINDOW_HEIGHT * 0.8f);
+    vertices[2].color    = sf::Color::Blue;
 
     while (window.isOpen())
     {
@@ -34,7 +45,7 @@ int main()
 
         window.clear();
 
-        window.draw(rect, &shader);
+        window.draw(vertices, &shader);
 
         window.display();
     }
